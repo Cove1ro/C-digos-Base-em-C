884247f238e3712e653f6873863c0ac04b1d71a6
Gabriel Ferreira <96669721+Cove1ro@users.noreply.github.com>
2023-10-07 17:27:06 -0300

    Add files via upload
    O código acima executa a sequência de fibonacci

---
+++ Fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int N1 = 1, N2 = 1, Cont = 2, S;
+    
+    printf("%d  %d ", N1, N2);
+    
+    while (Cont <= 20)
+    {
+        S = N1+N2;
+        N1 = N2;
+        N2 = S;
+        Cont++;
+        
+        printf(" %d ", N2);
+    }
+
+    return 0;
+}
+
